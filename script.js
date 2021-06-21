33c89ae4c0ff086f1916ce2dac8cf417fa80fa1b
shivani23212 <65727348+shivani23212@users.noreply.github.com>
2021-06-21 16:35:54 +0100

    Version 3
    Added backgrounds.

--- script.js
+++ script.js
@@ -6,9 +6,11 @@
 // use if..else if to bind multiple element changes to a single event (click)
 document.addEventListener('click', function(event) {
     if (event.target.innerHTML == "Dark Mode" ) {
-        document.documentElement.style.cssText = "--background-color: black; --text-color: white; --button-highlight: #3d3c37;";
+        document.documentElement.style.cssText = "--background-color: black; --background-image: url('dark_image.png'); --background-size: auto; --text-color: white; --button-highlight: #3d3c37;";
         document.getElementById('title').innerHTML="Dark Mode";
         document.getElementById('mode-btn').innerHTML="Light Mode";
+
+        
     }
     else if (event.target.innerHTML == "Light Mode") {
         document.documentElement.style.cssText = "--background-color: white; --text-color: black; --button-highlight: #e7e7e7;",
