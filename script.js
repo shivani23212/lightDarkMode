5f9bf838e48ce76303722abcc5c51b7f7ecd5d9b
shivani23212 <65727348+shivani23212@users.noreply.github.com>
2021-06-21 02:28:42 +0100

    Version 2
    Only 1 button.

--- script.js
+++ script.js
@@ -5,12 +5,14 @@
 
 // use if..else if to bind multiple element changes to a single event (click)
 document.addEventListener('click', function(event) {
-    if (event.target.id == ('dark-btn') ) {
+    if (event.target.innerHTML == "Dark Mode" ) {
         document.documentElement.style.cssText = "--background-color: black; --text-color: white; --button-highlight: #3d3c37;";
         document.getElementById('title').innerHTML="Dark Mode";
+        document.getElementById('mode-btn').innerHTML="Light Mode";
     }
-    else if (event.target.id == ('light-btn') ) {
+    else if (event.target.innerHTML == "Light Mode") {
         document.documentElement.style.cssText = "--background-color: white; --text-color: black; --button-highlight: #e7e7e7;",
         document.getElementById('title').innerHTML="Light Mode";
+        document.getElementById('mode-btn').innerHTML="Dark Mode";
     }
 }, false);
