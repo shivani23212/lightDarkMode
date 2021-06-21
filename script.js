61a471ace8e131cee60f6e362ef7163aaee6f77a
shivani23212 <65727348+shivani23212@users.noreply.github.com>
2021-06-22 00:54:32 +0100

    Version 4
    Changed backgrounds, allow user to enter their name - stored in cache.

--- script.js
+++ script.js
@@ -6,14 +6,25 @@
 // use if..else if to bind multiple element changes to a single event (click)
 document.addEventListener('click', function(event) {
     if (event.target.innerHTML == "Dark Mode" ) {
-        document.documentElement.style.cssText = "--background-color: black; --background-image: url('dark_image.png'); --background-size: auto; --text-color: white; --button-highlight: #3d3c37;";
+        document.documentElement.style.cssText = "--background-color: black; --background-image: url('dark_image2.jpg'); --background-size: auto; --text-color: white; --button-highlight: #3d3c37;";
         document.getElementById('mode-btn').innerHTML="Light Mode";
+        document.getElementById('mode-btn').style.color="orange";
+        document.getElementById('mode-btn').style.fontWeight="1000";
+
+
+        // change textbox bottom border and text if exists
+        if (document.getElementById("nameInput") != null) {
+            document.getElementById("nameInput").style.color = "white";
+            document.getElementById("nameInput").style.borderBottomColor = "white";
+        }
 
         
     }
     else if (event.target.innerHTML == "Light Mode") {
-        document.documentElement.style.cssText = "--background-color: white; --text-color: black; --button-highlight: #e7e7e7;";
+        document.documentElement.style.cssText = "--background-color: white; --background-image: url('light_image2.jpg'); --text-color: black; --button-highlight: #e7e7e7;";
         document.getElementById('mode-btn').innerHTML="Dark Mode";
+        document.getElementById('mode-btn').style.color="black"; // make more consise
+        document.getElementById('mode-btn').style.fontWeight="normal";
     }
 }, false);
 
@@ -32,25 +43,10 @@ function liveClock() {
 setInterval(liveClock, 1000); // runs function every second - update time
 
 
-// set personalised message (depending on name and time)
-setTimeout(function() {
-    var name = prompt("Please enter your name", " ");
-    var hour = today.getHours();
-    if (hour > 6 && hour <=12) {
-        document.getElementById("message").innerHTML="Good morning, "+name;
-    }
-    else if (hour > 12 && hour < 18) {
-        document.getElementById("message").innerHTML="Good afternoon, "+name;
-    }
-    else {
-        document.getElementById("message").innerHTML="Good evening, "+name;
-    }
-}, 500)
+var textbox = document.querySelector('input[type="text"]');
 
-
-
-window.onload = function() {
-    liveClock();
+// if name not in browser cache
+if (localStorage['myName'] == undefined || localStorage['myName'] == "") {
     var hour = today.getHours();
     if (hour > 6 && hour <=12) {
         document.getElementById("message").innerHTML="Good morning, ";
@@ -60,5 +56,38 @@ window.onload = function() {
     }
     else {
         document.getElementById("message").innerHTML="Good evening, ";
+    }
+
+
+// if name not in cache (hense there exists an input box for the blur event) get name when user clicks off text input box
+textbox.addEventListener('blur', (event) => {
+    var name = document.getElementById("nameInput").value; // set name of user
+
+    if (name != "") {
+        localStorage['myName'] = name; // only strings
+        document.getElementById("message").innerHTML+= localStorage['myName']; // add name to good morning/afternoon... msg
+        document.getElementById("nameInput").remove();
+    }
+
+  });
+}
+
+window.onload = function() {
+    liveClock();
+
+    // if name already in browser cache
+    if (localStorage['myName'] != undefined && localStorage['myName'] != "") {
+        var hour = today.getHours();
+        if (hour > 6 && hour <=12) {
+            document.getElementById("message").innerHTML="Good morning, "+localStorage['myName'];
+        }
+        else if (hour > 12 && hour < 18) {
+            document.getElementById("message").innerHTML="Good afternoon, "+localStorage['myName'];
+        }
+        else {
+            document.getElementById("message").innerHTML="Good evening, "+localStorage['myName'];
+        }
+
+        document.getElementById("nameInput").remove();
     }
 };
