b65a2a1548115fd4d427eabef1a5195831b84cb9
shivani23212 <65727348+shivani23212@users.noreply.github.com>
2021-06-22 14:09:13 +0100

    Version 5
    Clean Up

--- script.js
+++ script.js
@@ -26,6 +26,14 @@ document.addEventListener('click', function(event) {
         document.getElementById('mode-btn').style.color="black"; // make more consise
         document.getElementById('mode-btn').style.fontWeight="normal";
     }
+
+    else if (event.target.getAttribute("id") == "edit") {
+        var welcomeMsg = document.getElementById("message").innerHTML; // get 'good afternoon name' text
+        document.getElementById("message").innerHTML = welcomeMsg.substring(0, welcomeMsg.lastIndexOf(" ")); // removes name from text
+        document.getElementById("nameInput").style.display = "block"; // displays text input box
+        localStorage.removeItem("myName");
+        setName();
+    }
 }, false);
 
 
@@ -46,31 +54,39 @@ setInterval(liveClock, 1000); // runs function every second - update time
 var textbox = document.querySelector('input[type="text"]');
 
 // if name not in browser cache
-if (localStorage['myName'] == undefined || localStorage['myName'] == "") {
-    var hour = today.getHours();
-    if (hour > 6 && hour <=12) {
-        document.getElementById("message").innerHTML="Good morning, ";
-    }
-    else if (hour > 12 && hour < 18) {
-        document.getElementById("message").innerHTML="Good afternoon, ";
-    }
-    else {
-        document.getElementById("message").innerHTML="Good evening, ";
-    }
+let msg;
+function setName() {
+
+    if (localStorage['myName'] == undefined || localStorage['myName'] == "") {
+        document.getElementById("edit").style.display = "none";
+        var hour = today.getHours();
+        if (hour > 6 && hour <=12) {
+            msg = "Good morning, ";
+        }
+        else if (hour > 12 && hour < 18) {
+            msg = "Good afternoon, ";
+        }
+        else {
+            msg = "Good evening, ";
+        }
+        document.getElementById("message").innerHTML=msg;
 
 
-// if name not in cache (hense there exists an input box for the blur event) get name when user clicks off text input box
-textbox.addEventListener('blur', (event) => {
-    var name = document.getElementById("nameInput").value; // set name of user
+    // if name not in cache (hense there exists an input box for the blur event) get name when user clicks off text input box
+    textbox.addEventListener('blur', (event) => {
+        var name = document.getElementById("nameInput").value; // set name of user
 
-    if (name != "") {
-        localStorage['myName'] = name; // only strings
-        document.getElementById("message").innerHTML+= localStorage['myName']; // add name to good morning/afternoon... msg
-        document.getElementById("nameInput").remove();
+        if (name != "") {
+            localStorage['myName'] = name; // only strings
+            document.getElementById("message").innerHTML= msg+localStorage['myName']; // add name to good morning/afternoon... msg
+            document.getElementById("nameInput").style.display = "none"; 
+            document.getElementById("edit").style.display = "block";
+        }
+
+    });
     }
-
-  });
 }
+setName();
 
 window.onload = function() {
     liveClock();
@@ -87,7 +103,7 @@ window.onload = function() {
         else {
             document.getElementById("message").innerHTML="Good evening, "+localStorage['myName'];
         }
-
-        document.getElementById("nameInput").remove();
+        document.getElementById("nameInput").style.display = "none";
+        document.getElementById("edit").style.display = "block";
     }
 };
